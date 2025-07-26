ac1f3ab58819df7700be703dc76d552e4f63d635
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-07-26 07:47:08 +0700

    feat:update data

---
+++ kafka-cli-playground1/02-kafka-101/02-producer.sh
@@ -0,0 +1,2 @@
+# to produce messages
+kafka-console-producer --bootstrap-server localhost:9092 --topic hello-world
---
+++ kafka-cli-playground1/02-kafka-101/03-consumer.sh
@@ -0,0 +1,4 @@
+# to consume messages
+kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic hello-world
+# to consume messages from the beginning
+kafka-console-consumer.sh --bootstrap-server localhost:9092 --topic hello-world --from-beginning
